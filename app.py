1b72b65b1654d6bfd8767cd27862f029781cb3ac
juntaolei <44855944+juntaolei@users.noreply.github.com>
2019-10-23 08:40:16 -0400

    Working on auth and routes

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 # Standard Lib
 from os import urandom
 # Flask Lib
-from flask import Flask, g
+from flask import Flask, g, session, redirect, url_for
 
 # Custom Modules
 from utl.dbconn import conn, close
+from utl.auth import auth
 
 # Initialize Flask app that stores a reference to a database file
 app = Flask(__name__)
@@ -24,8 +25,17 @@ def close_database_connection(ex):
 # Testing at the moment
 @app.route("/")
 def index():
-  print(g.db)
-  return "index"
+  if "usr" in session:
+    redirect(url_for("home"))
+  redirect(url_for("login"))
+
+@app.route("/login")
+def login():
+  return "login"
+
+@app.route("/home")
+def home():
+  return "home"
 
 # Executes the Flask app if this file is the main file
 if __name__ == "__main__":
--- utl/auth.py
+++ utl/auth.py
@@ -1,2 +1,12 @@
 # Standard Lib
-from hashlib import sha512
+from hashlib import sha256
+# Flask Lib
+from flask import g
+
+def hash(userid, password):
+  return sha256(hex(userid) + password).hexdigest()
+
+def auth(userid, password, hash):
+  if hash(userid, password) == hash:
+    return True
+  return False

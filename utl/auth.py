8ce88f410907c4b64d4e7e72337cac7ff5afa0f0
juntaolei <44855944+juntaolei@users.noreply.github.com>
2019-10-24 23:03:11 -0400

    basic login working so far

--- utl/auth.py
+++ utl/auth.py
@@ -1,18 +1,23 @@
 # Standard Lib
 from hashlib import sha256
 # Flask Lib
-from flask import g
+from flask import current_app, g
 
 # User Modules
-from . import dbfunc
+from .dbfunc import insert, get
 
-def get_hash(salt, password):
-  return sha256((salt + password).encode()).hexdigest()
 
-def auth(password, salt, hashed):
-  if get_hash(salt, password) == hashed:
+def get_hash(password):
+    return sha256((current_app.config["SALT"] + password).encode()).hexdigest()
+
+
+def auth(username, password):
+    if get_hash(password) == get("users", "hashpassword", "WHERE username = '%s'" % (username)):
+        return True
+    return False
+
+
+def register(username, password):
+    if not auth(username, password):
+        insert("users", [username, get_hash(password), username])
     return True
-  return False
-
-def register(usrname, passwd):
-  return True
--- utl/dbfunc.py
+++ utl/dbfunc.py
@@ -1,24 +1,30 @@
 # Flask Lib
 from flask import g
 
+
 def header_types(tbl_name):
-  cur = g.db.cursor()
-  cur.execute("PRAGMA TABLE_INFO (%s)" % (tbl_name))
-  heads = cur.fetchall()
-  cur.close()
-  return [str(head[1]) for head in heads]
+    cur = g.db.cursor()
+    cur.execute("PRAGMA TABLE_INFO (%s)" % (tbl_name))
+    heads = cur.fetchall()
+    cur.close()
+    return [str(head[1]) for head in heads]
+
 
 def insert(tbl_name, values):
-  try:
-    g.db.execute('INSERT INTO %s VALUES (%s)' % (tbl_name, ",".joing(values)))
-    g.db.commit()
-    return True
-  except:
-    return False
+    try:
+        cur = g.db.cursor()
+        cur.execute("INSERT INTO %s VALUES (NULL,'%s')" %
+                    (tbl_name, "','".join(values)))
+        g.db.commit()
+        return True
+    except:
+        return False
 
-def get(tbl_name, query, identifier):
-  cur = g.db.cursor()
-  cur.execute('SELECT %s FROM %s WHERE %s = %s' % (query, tbl_name, identifier))
-  values = cur.fetchall()
-  cur.close()
-  return ",".join([value for value in values])
+
+def get(tbl_name, column, conditional=""):
+    cur = g.db.cursor()
+    print("SELECT %s FROM %s %s" % (column, tbl_name, conditional))
+    cur.execute("SELECT %s FROM %s %s" % (column, tbl_name, conditional))
+    values = cur.fetchall()
+    cur.close()
+    return ",".join([str(value[0]) for value in values])

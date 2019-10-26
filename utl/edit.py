53cc31cda6c380cbb3eb8d5e6d715b399ee4f54a
Sophie Nichol <snichol00@stuy.edu>
2019-10-25 20:01:01 -0400

    added delete post method

---
+++ utl/edit.py
@@ -0,0 +1,16 @@
+# User Modules
+from .dbfunc import insert, get
+from flask import g
+
+def new_post(user_id, author):
+    insert("blogs", [user_id, author])
+
+def update_post(blog_id, blog_title, blog_content, time_stamp):
+    cur = g.db.cursor()
+    cur.execute("UPDATE blogs SET title = blog_title, content = blog_content, last_updated = time_stamp WHERE blogid = blog_id")
+    g.db.commit()
+
+def delete_post(id):
+    cur = g.db.cursor()
+    cur.execute("DELETE FROM blogs WHERE blogid = '{}';".format(id))
+    g.db.commit()
